da61c25cba8e838a62b30ab0309d867535814188
José Diaz <xoxefdp@gmail.com>
2019-04-30 13:18:57 +0200

    Initial commit with ecma webpack starter boilerplate

---
+++ webpack/webpack.common.js
@@ -0,0 +1,44 @@
+const Path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.js')
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: '[name].bundle.js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/index.html'),
+      favicon: Path.resolve(__dirname, '../src/favicon.ico')
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      }
+    ]
+  }
+}
